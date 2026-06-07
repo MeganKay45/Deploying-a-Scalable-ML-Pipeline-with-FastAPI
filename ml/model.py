1782b20c6077597dd859c711bf57ecceb0c4cfa0
MeganKay45 <mkwhite45@yahoo.com>
2026-06-07 16:00:43 +0000

    Update train_model

--- ml/model.py
+++ ml/model.py
@@ -1,4 +1,5 @@
 import pickle
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
@@ -19,6 +20,10 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+    return model
+
     # TODO: implement the function
     pass
 
@@ -50,7 +55,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : trained sklearn model
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -59,6 +64,9 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
+    preds = model.predict(X)
+    return preds
+
     # TODO: implement the function
     pass
 
@@ -72,11 +80,18 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
+    with open(path, "wb") as f:
+        pickle.dump(model, f)
+
     # TODO: implement the function
     pass
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
+    with open(path, "rb") as f:
+        model = pickle.load(f)
+    return model
+
     # TODO: implement the function
     pass
 
@@ -84,6 +99,21 @@ def load_model(path):
 def performance_on_categorical_slice(
     data, column_name, slice_value, categorical_features, label, encoder, lb, model
 ):
+    sliced_data = data[data[column_name] == slice_value]
+
+    X_slice, y_slice, _, _ = process_data(
+        sliced_data,
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb,
+    )
+
+    preds = inference(model, X_slice)
+    precision, recall, fbeta = compute_model_metrics(y_slice, preds)
+    return precision, recall, fbeta
+
     """ Computes the model metrics on a slice of the data specified by a column name and
 
     Processes the data using one hot encoding for the categorical features and a
